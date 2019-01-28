5955f4a21c00e8219a87005c9f8689160e5cd543
Lincoln Thurlow <lincoln@isi.edu>
2019-01-28 15:18:09 -0800

    nex-branch: added pxe boot options for nex

--- install_nex.sh
+++ install_nex.sh
@@ -1,3 +1,8 @@
 sudo ansible-playbook -i .rvn/ansible-hosts install_etcd.yml
 
-sudo ansible-playbook -i .rvn/ansible-hosts --extra-vars "etcd.cacert='' etcd.cacert='' etcd.key='' domain=test.net interface=eth1" install_nex.yml
+sudo ansible-playbook -i .rvn/ansible-hosts --extra-vars \
+	"domain=test.net interface=eth1" install_nex.yml
+
+serv=`rvn ip server`
+ssh -o StrictHostKeyChecking=no -i /var/rvn/ssh/rvn rvn@$serv \
+	"sudo nex apply /etc/nex/sled.yml"
